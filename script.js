674f1649b2f7544e1dc2b3e9f0cc9d816e4fb8df
Sarah Barajas <sarahbarajas91@yahoo.com>
2024-11-13 20:59:04 -0800

    generic error message

--- script.js
+++ script.js
@@ -13,8 +13,31 @@ myAverage = myAverage / myGrades.length;
 console.log("My grade average is " + myAverage);
 // exception class for invalid grades
 class InvalidGradeException extends Error{
-    { constructor(message) {
+    { constructor(message); {
         super(message);
         this.name = "InvalidGradeExpectations";
     }
 }
+// function to validate a grade input
+function validateGrade(grade) {
+    //check if input is a number
+    if (typeof grade !== 'number') { throw new InvalidGradeException("Input must be a number");
+     }
+     // check if grade is between 0 and 100
+     if (grade < 0 || grade > 100) { throw new InvalidGradeException("Grade must be between 0 and 100");
+    } 
+    return true;
+}
+// try-catch block to handle exceptions
+try {
+    //test validateGrade function with an invalid value
+    validateGrade(105); //change the value 
+} catch(e){
+    if (e instanceof InvalidGradeException)}
+        //output the custom exception message 
+        console.error(e.message);
+    } else {
+        // output generic error message for unexpected error
+        console.error("An unexpected error occurred:", e);
+    }
+
